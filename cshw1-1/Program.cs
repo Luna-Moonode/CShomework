330494808b1b7cac2dfa8ed1fe2945c6a45aed92
Moonode <55302867+Luna-Moonode@users.noreply.github.com>
2021-03-09 23:51:50 +0800

    Add files via upload

---
+++ cshw1-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace cshw1_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = Convert.ToDouble(Console.ReadLine());
+            var o = Console.ReadLine();
+            var b = Convert.ToDouble(Console.ReadLine());
+            double c;
+            switch (o)
+            {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+                default:
+                    Console.WriteLine("invalid");
+                    return;
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
