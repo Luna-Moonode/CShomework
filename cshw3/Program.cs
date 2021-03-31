bda391206ad451ad6c2411498c7e9ff5e394b5af
Moonode <55302867+Luna-Moonode@users.noreply.github.com>
2021-03-31 09:58:07 +0800

    3

---
+++ cshw3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cshw3
+{
+    internal abstract class Shape
+    {
+        public abstract string Name { get; }
+        public abstract double Area { get; }
+    }
+    
+    internal interface IValidator
+    {
+        bool IsValid();
+    }
+
+    internal class Rectangle : Shape
+    {
+        private readonly double _length;
+        private readonly double _width;
+        public double length => _length;
+        public Rectangle(int length, int width) 
+        {
+            _length = length;
+            _width = width;
+        }
+        public override string Name => "长方形";
+        public override double Area => _length * _width;
+    }
+
+    internal class Square : Shape
+    {
+        private readonly double _side;
+        public double side => _side;
+        public Square(double side)
+        {
+            _side = side;
+        }
+
+        public override string Name => "正方形";
+        public override double Area => _side * _side;
+    }
+
+    internal class Triangle : Shape, IValidator
+    {
+        private readonly double[] _sides = {0, 0, 0};
+        public double[] sides => _sides;
+        public Triangle(double side1, double side2, double side3)
+        {
+            _sides[0] = side1;
+            _sides[1] = side2;
+            _sides[2] = side3;
+            Validate();
+        }
+
+        public override string Name => "三角形";
+
+        public override double Area
+        {
+            get
+            {
+                var p = (_sides[0] + _sides[1] + _sides[2]) / 2.0;
+                return Math.Sqrt(p * (p - _sides[0]) * (p - _sides[1]) * (p - _sides[2]));
+            }
+        }
+        public bool IsValid()
+        {
+            var flag =  sides[0] + sides[1] > sides[2] &&
+                   sides[0] + sides[2] > sides[1] &&
+                   sides[1] + sides[2] > sides[0];
+            return flag;
+        }
+        private void Validate()
+        {
+            if (!IsValid()) throw new Exception();
+        }
+    }
+
+    internal abstract class Factory
+    {
+        void Generate() {}
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var a = new Triangle(3, 4, 5);
+            Console.WriteLine(a.sides);
+        }
+    }
+}
---
+++ cshw3/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("cshw3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cshw3")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("B9E03C04-D1D6-4020-B4F0-D71B5AC6AEC1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
